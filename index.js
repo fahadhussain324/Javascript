6b488f166fb0c7d0ddcefa2dda3df28771e764c5
Fahad Hussain <fahadhussain324@outlook.com>
2025-04-26 15:14:42 +0100

    Initial commit. HTML, Js and Css files created. No Js functionality in this initial push.

---
+++ index.js
@@ -0,0 +1,11 @@
+
+// user input
+
+//window prompt is easy, html textbox is professional way
+
+// let username = window.prompt("What is your username? ");
+// console.log(username);
+
+//Site version
+let siteVersion = 1;
+document.getElementById("version").textContent(`Version: ${siteVersion}`);
